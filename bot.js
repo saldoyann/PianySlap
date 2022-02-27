4fdd1e31a23eb5a8d7ac4fe14cad010e9ceeaaf0
Yann SALDO <yann.saldo@gmail.com>
2022-02-27 20:41:16 +0100

    more env var + install express

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+const express = require('express');
+const app = express();
 const Discord = require('discord.js');
 const client = new Discord.Client({ 
   intents: [
@@ -7,13 +9,22 @@ const client = new Discord.Client({
   ] 
 });
 
+app.set('port', (process.env.PORT));
+
+app.get('/', function(request, response) {
+  var result = 'App is running'
+  response.send(result);
+}).listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 client.on("ready", () => {
   console.log("PianySlap Bot is ready to go !")
 })
 
 client.on("messageCreate", msg => {
   if(msg.author.bot){
-    if(msg.author.username == 'MEE6'){
+    if(msg.author.username == process.env.BOT_NAME){
       if(msg.embeds[0].description.indexOf('Bad word usage') > -1)
         msg.reply(process.env.GIF_SLAP);
     }    
