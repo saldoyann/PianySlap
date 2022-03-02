18a65330b0b6f063adefbac5beba0d1a1b974598
foucaud_e <efoucaud@ifotec.com>
2022-03-02 22:20:08 +0100

    Refactor: Prettier

--- bot.js
+++ bot.js
@@ -1,40 +1,42 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const Discord = require('discord.js');
-const client = new Discord.Client({ 
-  intents: [
-    Discord.Intents.FLAGS.GUILDS,
-    Discord.Intents.FLAGS.GUILD_MESSAGES
-  ] 
+const Discord = require("discord.js");
+const client = new Discord.Client({
+  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
 });
 
-app.set('port', (process.env.PORT));
+app.set("port", process.env.PORT);
 
-app.get('/', function(request, response) {
-  var result = 'App is running'
-  response.send(result);
-}).listen(app.get('port'), function() {
-  console.log('App is running, server is listening on port ', app.get('port'));
-});
+app
+  .get("/", function (request, response) {
+    var result = "App is running";
+    response.send(result);
+  })
+  .listen(app.get("port"), function () {
+    console.log(
+      "App is running, server is listening on port ",
+      app.get("port")
+    );
+  });
 
 client.on("ready", () => {
-  console.log("PianySlap Bot is ready to go !")
-})
+  console.log("PianySlap Bot is ready to go !");
+});
 
-client.on("messageCreate", msg => {
+client.on("messageCreate", (msg) => {
   // Slap is a meme from N0lito about links in discord
-  if(msg.author.bot){
-    if(msg.author.username == process.env.BOT_NAME){
-      if(msg.embeds[0].description.indexOf(process.env.DESCR) > -1)
+  if (msg.author.bot) {
+    if (msg.author.username == process.env.BOT_NAME) {
+      if (msg.embeds[0].description.indexOf(process.env.DESCR) > -1)
         msg.reply(process.env.GIF_SLAP);
-    }    
+    }
     return;
   }
   // 🐦 is a meme from Clyde le Rouge and french community
-  if(msg.content == "bouird") {
-    msg.react('🐦')
+  if (msg.content == "bouird") {
+    msg.react("🐦");
   }
 });
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
